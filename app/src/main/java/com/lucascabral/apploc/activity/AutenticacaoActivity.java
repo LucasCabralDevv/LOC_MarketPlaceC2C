04710fbe0349278ba0f3854fe5a6b9d58f581bb6
lucascabraldeve <lucas_cabral.deve@outlook.com>
2020-11-30 13:41:20 -0300

    Criado uma splash screem e a activity de autenticação.

--- app/src/main/java/com/lucascabral/apploc/activity/AutenticacaoActivity.java
+++ app/src/main/java/com/lucascabral/apploc/activity/AutenticacaoActivity.java
@@ -1,14 +1,16 @@
-package com.lucascabral.apploc;
+package com.lucascabral.apploc.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.lucascabral.apploc.R;
+
+public class AutenticacaoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_autenticacao);
     }
 }
---
+++ app/src/main/java/com/lucascabral/apploc/activity/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.lucascabral.apploc.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.lucascabral.apploc.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        abrirAutenticacao();
+                    }
+                }, 3000);
+    }
+
+    private void abrirAutenticacao(){
+
+        Intent intent = new Intent(SplashActivity.this, AutenticacaoActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
